3609d7493faf63df9e763001315121a9aa3b19c3
Будаев Иван <budaevivan2003@gmail.com>
2023-06-13 21:15:50 +0500

    Homework 4 done (MoveLeft.cs + MoveLeftTransformer.cs)

---
+++ Homework4/PhotoEnhancer/PhotoEnhancer/Filters/Transform/MoveLeft.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class MoveLeft : IParameters
+    {
+        public double NInPerсents { get; set; }
+        public ParameterInfo[] GetDecription()
+        {
+            return new[]
+            {
+                new ParameterInfo()
+                {
+                    Name = "Сдвиг в %",
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 0,
+                    Increment = 5
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            NInPerсents = values[0];
+        }
+    }
+}
---
+++ Homework4/PhotoEnhancer/PhotoEnhancer/Filters/Transform/MoveLeftTransformer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class MoveLeftTransformer : ITransformer<MoveLeft>
+    {
+        double N { get; set; }
+
+        public Size Size { get; private set; }
+
+        public void Initialize(Size size, MoveLeft parameters)
+        {
+            N = parameters.NInPerсents /100;
+            Size = size;
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            point = new Point(point.X, point.Y);
+            var x = (int)(point.X + (Size.Width*N));
+            var y = (int)(point.Y);
+
+            if (x >= Size.Width)
+                x -= Size.Width;
+
+            return new Point(x, y);
+        }
+    }
+}
